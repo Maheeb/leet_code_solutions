3b3bdd18556a4ca09efffc4dbff65ead2c4a423a
maheeb <maheeb009@gmail.com>
2023-04-22 23:33:15 +0600

    updated

--- 53. Maximum Subarray.js
+++ 53. Maximum Subarray.js
@@ -14,5 +14,5 @@ var maxSubArray = function(nums) {
 
 };
 
-let nums = [-2,1,-3,4,-1,2,1,-5,4]
-console.log(maxSubArray(nums))
+// let nums = [-2,1,-3,4,-1,2,1,-5,4]
+// console.log(maxSubArray(nums))
