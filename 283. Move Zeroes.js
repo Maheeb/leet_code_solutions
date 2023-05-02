9e402ded98e5a37ab9e789d21682fd8a655a5289
Maheeb <maheeb009@gmail.com>
2023-05-02 23:57:37 +0600

    updated

--- 283. Move Zeroes.js
+++ 283. Move Zeroes.js
@@ -2,6 +2,31 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var moveZeroes = function(nums) {
+//   return  nums.sort((a, b) => a === 0 ? 1 : b === 0 ? -1 : 0);
+// };
+
 var moveZeroes = function(nums) {
-  return  nums.sort((a, b) => a === 0 ? 1 : b === 0 ? -1 : 0);
+
+    let nonZeroIndex = 0;
+    for(let i=0; i<nums.length; i++)
+    {
+        if(nums[i] !==0)
+        {
+            nums[nonZeroIndex] = nums[i];
+            nonZeroIndex++;
+        }
+    }
+  for(let i=nonZeroIndex; i<nums.length; i++)
+  {
+      nums[i] =0;
+  }
+
+
 };
+
+// let nums = [0,1]
+// let nums = [0,1,0,3,12]
+// let nums = [0,1,0,3,12]
+//
+// console.log(moveZeroes(nums))
--- 344. Reverse String.js
+++ 344. Reverse String.js
@@ -2,8 +2,26 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+// var reverseString = function(s) {
+//     return  s.reverse();
+// };
+
 var reverseString = function(s) {
-    return  s.reverse();
+    let result = [];
+    let length = s.length
+    for (let i = s.length-1; i >=0 ; i--) {
+
+      // s.shift(i)
+        s.push(s[i])
+        // s.splice(i, 1, s[i]);
+    }
+
+    for (let i = 0; i < length; i++) {
+        s.shift(i)
+    }
+    // console.log(s)
 };
+
+
 let s = ["h","e","l","l","o"]
-reverseString(s)
+console.log(reverseString(s))
