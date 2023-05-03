be11e3cda4b5c2bd0c723b78f90d1d95607bb7d3
Maheeb <maheeb009@gmail.com>
2023-05-03 22:33:32 +0600

    updated

--- 557. Reverse Words in a String III.js
+++ 557. Reverse Words in a String III.js
@@ -2,14 +2,25 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    let splitString = s.split(' ');
-    let reverseWords = splitString.map((item, i) =>{
-        return item.split('').reverse().join('');
-    })
+// var reverseWords = function(s) {
+//     let splitString = s.split(' ');
+//     let reverseWords = splitString.map((item, i) =>{
+//         return item.split('').reverse().join('');
+//     })
+//
+//     return reverseWords.join(' ');
+// };
 
-    return reverseWords.join(' ');
+var reverseWords = function(s) {
+
+    let splitString = s.split("").reverse().join('');
+    let result = [];
+    let splitStringArray = splitString.split(' ')
+    return splitStringArray.reverse().join(' ')
+
 };
 
-let s = "Let's take LeetCode contest";
+
+let s = "God Ding";
+// let s = "Let's take LeetCode contest"
 console.log(reverseWords(s));
