92bea84795ab0963c7234f03a8d251138627d5da
Maheeb <maheeb009@gmail.com>
2023-05-16 01:19:15 +0600

    updated

--- 605. Can Place Flowers.js
+++ 605. Can Place Flowers.js
@@ -4,5 +4,19 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
+    let i =0;
+
+    while (i < flowerbed.length && n!==0)
+    {
+        if (flowerbed[i]===0 && flowerbed[i+1] !==1 && flowerbed[i-1] !==1)
+        {
+            n--;
+            i++;
+        }
+        i++;
+    }
+    return n===0;
 
 };
+let flowerbed = [1,0,0,0,1], n = 1;
+console.log(canPlaceFlowers(flowerbed, n));
