bc1676237955bbdbd3b96eccc8473e55117f4728
Maheeb <maheeb009@gmail.com>
2023-05-17 01:42:31 +0600

    updated

--- 1299. Replace Elements with Greatest Element on Right Side.js
+++ 1299. Replace Elements with Greatest Element on Right Side.js
@@ -3,5 +3,12 @@
  * @return {number[]}
  */
 var replaceElements = function(arr) {
-
+    let newArr = [];
+    newArr[arr.length-1] = -1;
+    for (let i = arr.length-2; i >= 0; i--) {
+        newArr[i] = Math.max(newArr[i+1],arr[i+1]);
+    }
+    return newArr;
 };
+let arr = [17,18,5,4,6,1]
+console.log(replaceElements(arr))
